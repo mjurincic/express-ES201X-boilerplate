333014d544f9e90ce67e17fecd557b51103b2833
Mauro <mauro@jurincic.net>
2018-02-27 01:10:02 +0100

    Init project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'dotenv-extended/config';
+import app from './app';
+
+const { PORT = 8080 } = process.env;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
